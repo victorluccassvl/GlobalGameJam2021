4d955ff2670f8fd4b5ceab6d589abf103d439a95
MIguel <mglguida1@gmail.com>
2021-01-30 02:16:15 -0300

    Add Door script - open door

---
+++ Assets/Scripts/Door/OpenDoor.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+    private PickSoul _pickedSoul = null; // reference to soul
+
+    [SerializeField]
+    private bool _opened = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            _pickedSoul = other.GetComponent<PickSoul>();
+            Debug.Assert(_pickedSoul != null, "The player does not have a soul picker");
+            if (_pickedSoul.HasSoul)
+            {
+                _opened = true;
+                _pickedSoul.enabled = false;
+                print("A porta abriu");
+            }
+        }
+    }
+
+    private void Update()
+    {
+        
+    }
+}
