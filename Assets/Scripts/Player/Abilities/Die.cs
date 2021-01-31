ee2b6736d9c3813f7062dbaff9644e804443e862
victorluccassvl <victor.luccas.svl@gmail.com>
2021-01-31 15:37:34 -0300

    no message

--- Assets/Scripts/Player/Abilities/Die.cs
+++ Assets/Scripts/Player/Abilities/Die.cs
@@ -6,13 +6,14 @@ public class Die : MonoBehaviour
 {
     private Player _player = null;
 
-    public const float X_spawn = -21f, Y_spawn = 1f, Z_spawn = 0f;
+    [SerializeField]
+    private Transform _spawn = null;
 
     public void ToDie()
     {
         _player = GetComponent<Player>();
         _player.PlayPlayerSound(PlaySound.Audios.Die);
-        _player.transform.position = new Vector3(X_spawn,Y_spawn,Z_spawn);
+        _player.transform.position = _spawn.position;
     }
 }
 
